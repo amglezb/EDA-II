dc001296f9a113db3bdba021ad4c9d7e340593e9
amglezb <amglez.edu@gmail.com>
2025-10-22 19:41:54 -0600

    p9_vfinal

---
+++ NOTAS/programas_omp/ej1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    #pragma omp parallel
+    {
+        int id_hilo = omp_get_thread_num();
+        int total_hilos = omp_get_num_threads();
+
+        #pragma omp critical
+        {
+            printf("Hola Mundo desde el hilo %d de %d\n", id_hilo, total_hilos);
+        }
+    }
+    return 0;
+}
