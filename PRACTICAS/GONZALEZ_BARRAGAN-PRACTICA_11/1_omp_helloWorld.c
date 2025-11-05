1b91d5fffeeaa403ce876421bb24383b8374c53e
amglezb <amglez.edu@gmail.com>
2025-11-05 10:30:50 -0600

    oct-5

---
+++ PRACTICAS/GONZALEZ_BARRAGAN-PRACTICA_11/1_omp_helloWorld.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<omp.h>
+
+int main() {
+    #pragma omp parallel
+    {
+        int ID = omp_get_thread_num();
+        printf("Hello(%d)", ID);
+        printf("world!!(%d)\n", ID);
+    }
+    return 0;
+}
+
+// Para Compilar
+//  gcc–fopenmp name.c-o name
